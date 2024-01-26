fa02a7d941bbd4bfa0074ceb2c30392f1a517342
tarush-s <tarushs@seas.upenn.edu>
2024-01-26 18:29:00 +0000

    Work in progress

--- hw1/rca.sv
+++ hw1/rca.sv
@@ -23,7 +23,7 @@ module fulladder(input wire  cin,
                  output wire s,
                  output wire cout);
    wire s_tmp, cout_tmp1, cout_tmp2;
-   halfadder h0(.a(a), .b(b), .s(cout), .cout(cout_tmp1));
+   halfadder h0(.a(a), .b(b), .s(s_tmp), .cout(cout_tmp1));
    halfadder h1(.a(s_tmp), .b(cin), .s(s), .cout(cout_tmp2));
    assign cout = cout_tmp1 | cout_tmp2;
 endmodule
@@ -33,10 +33,10 @@ module fulladder2(input wire        cin,
                   input wire  [1:0] a,
                   input wire  [1:0] b,
                   output wire [1:0] s,
-                  output wire       cout);
+                  output wire cout);
    wire cout_tmp;
-   fulladder a0(.cin(cin), .a(a[0]), .b(b[0]), .s(s[0]), .cout(cout));
-   fulladder a1(.cin(cout_tmp), .a(a[1]), .b(b[1]), .s(s[0]), .cout(cout_tmp));
+   fulladder a0(.cin(cin), .a(a[0]), .b(b[0]), .s(s[0]), .cout(cout_tmp));
+   fulladder a1(.cin(cout_tmp), .a(a[1]), .b(b[1]), .s(s[1]), .cout(cout));
 endmodule
 
 /* 4-bit ripple-carry adder that adds two 4-bit numbers (taken from the
@@ -46,6 +46,6 @@ module rca4(input wire  [7:0] SWITCH,
             output wire [7:0] LED);
    wire cout0, ignored;
    fulladder2 a0(.cin(1'b1), .a(SWITCH[1:0]), .b(SWITCH[5:4]), .s(LED[1:0]), .cout(cout0));
-   fulladder2 a3(.cin(cout0), .a(SWITCH[3:1]), .b(SWITCH[7:6]), .s(LED[3:2]), .cout(ignored));
+   fulladder2 a3(.cin(cout0), .a(SWITCH[2:3]), .b(SWITCH[6:7]), .s(LED[3:2]), .cout(ignored));
    assign LED[7:4] = 4'h0;
 endmodule
