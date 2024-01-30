7f700ee52141e6bc5817d7f78d0cf55384e8ad9b
tarush-s <tarush.s148@gmail.com>
2024-01-30 01:19:20 +0000

    DU

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -12,6 +12,29 @@ module divider_unsigned (
 );
 
     // TODO: your code here
+    wire[31:0] temp_output_dividend;
+    wire[31:0] temp_output_remainder;
+    wire[31:0] temp_output_quotient;
+    wire[31:0] temp_input_remainder;
+    wire[31:0] temp_input_quotient;
+
+    for(int i = 0; i < 32; i++) begin 
+        if( i == 0)begin 
+            temp_input_remainder = 32'b0;
+            temp_input_quotient = 32'b0;
+            ivu_1iter a0 (.i_dividend(i_dividend), .i_divisor(i_divisor), .i_remainder(temp_input_remainder), 
+                          .i_quotient(temp_input_quotient), .o_dividend(temp_output_dividend), .o_remainder(temp_output_remainder)
+                          .o_quotient(temp_output_quotient));
+        end
+        else begin
+            ivu_1iter a1 (.i_dividend(temp_output_dividend), .i_divisor(i_divisor), .i_remainder(temp_output_remainder)
+                          .i_quotient(temp_output_quotient), o_dividend(temp_output_dividend), o_remainder(temp_output_remainder)
+                          .o_quotient(temp_output_quotient));
+        end
+    end 
+
+    assign o_remainder = temp_output_remainder;
+    assign o_quotient = temp_output_quotient; 
 
 endmodule
 
