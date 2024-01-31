0410d095c960999191f133361d36f8d7d3aecc8f
tarush-s <tarush.s148@gmail.com>
2024-01-31 19:08:39 +0000

    finished hw2a

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -62,33 +62,30 @@ module divider_unsigned (
     wire [31:0] temp_output_dividend[31:0];
     wire [31:0] temp_output_remainder[31:0];
     wire [31:0] temp_output_quotient[31:0];
-    wire [31:0] temp_input_remainder;
-    wire [31:0] temp_input_quotient;
+    wire [31:0] temp_input_remainder = 0;
+    wire [31:0] temp_input_quotient = 0;
+    genvar i;
 
-    always_comb begin
-        for(int i = 0; i < 32; i++) begin 
-            if( i == 0)begin 
-                temp_input_remainder = 32'b0;
-                temp_input_quotient = 32'b0;
-                divu_1iter a1(.i_dividend(i_dividend), 
-                             .i_divisor(i_divisor), 
-                             .i_remainder(temp_input_remainder), 
-                             .i_quotient(temp_input_quotient), 
-                             .o_dividend(temp_output_dividend[0]), 
-                             .o_remainder(temp_output_remainder[0]), 
-                             .o_quotient(temp_output_quotient[0]));
-            end
-            else begin
-                divu_1iter a2(.i_dividend(temp_output_dividend[i-1]), 
-                             .i_divisor(i_divisor), 
-                             .i_remainder(temp_output_remainder[i-1]),
-                             .i_quotient(temp_output_quotient[i-1]), 
-                             .o_dividend(temp_output_dividend[i]), 
-                             .o_remainder(temp_output_remainder[i]), 
-                             .o_quotient(temp_output_quotient[i]));
-            end
-        end 
-    end
+    for(i = 0; i < 32; i=i+1) begin : gen32divider
+        if( i == 0)begin : zeroth_iter
+            divu_1iter d1(.i_dividend(i_dividend), 
+                            .i_divisor(i_divisor), 
+                            .i_remainder(temp_input_remainder), 
+                            .i_quotient(temp_input_quotient), 
+                            .o_dividend(temp_output_dividend[0]), 
+                            .o_remainder(temp_output_remainder[0]), 
+                            .o_quotient(temp_output_quotient[0]));
+        end : zeroth_iter
+        else begin : later_iter
+            divu_1iter d2(.i_dividend(temp_output_dividend[i-1]), 
+                            .i_divisor(i_divisor), 
+                            .i_remainder(temp_output_remainder[i-1]),
+                            .i_quotient(temp_output_quotient[i-1]), 
+                            .o_dividend(temp_output_dividend[i]), 
+                            .o_remainder(temp_output_remainder[i]), 
+                            .o_quotient(temp_output_quotient[i]));
+        end : later_iter
+    end : gen32divider
 
     assign o_remainder = temp_output_remainder[31];
     assign o_quotient = temp_output_quotient[31]; 
